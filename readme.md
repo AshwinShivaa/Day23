b462f4a0e8cba6c7e3c277e291021c1e478de470
AshwinShivaa <ashwinshivaaduraisamy@gmail.com>
2024-08-10 17:09:02 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# React Todo Application
+
+This is a Todo Application built with React.js that allows users to manage their tasks with CRUD (Create, Read, Update, Delete) functionalities. Users can create new tasks, edit existing ones, change task status, and filter tasks based on their status.
+
+
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+  return (
+    <>
+    <footer className="py-5 mt-3 bg-dark">
+        <div className="container">
+            <p className="m-0 text-center text-white">
+                Copyright @Ashwin Shivaa 2024
+            </p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
